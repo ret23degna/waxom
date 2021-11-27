6d235616bfe6834b0580c59da2c91118e33f0a23
ret23degna <ret23degna@gmail.com>
2021-11-27 18:13:45 +0500

    waxom

---
+++ app.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	/*Reviews*/
+    let slider=$("#reviewsSlider");
+
+    slider.slick({
+       infinite: true,
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       fade: true,
+       arrows:false
+    });
+    
+});
